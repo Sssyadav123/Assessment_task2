55c8af7585265952ea484819c3d0fc9c05b310f1
Sssyadav123 <sumityadav25199.com>
2025-02-24 21:41:36 +0530

    last_commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Deployed Using Github-actions on eks cluster');
 });
 
 app.listen(PORT, HOST, () => {
